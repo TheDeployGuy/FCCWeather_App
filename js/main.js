4cf09775db9b90a37ee8c47081640a31261326cd
Jason Lloyd <jason.lloyddev@gmail.com>
2016-12-22 20:19:36 +0000

    Final Checkin - Project submitted to FCC
    - Clean up duplicate code
    - Final visual touches

--- js/main.js
+++ js/main.js
@@ -10,52 +10,51 @@ app1.controller('main',function($scope,$http,$sce){
 
   var api = "http://api.openweathermap.org/data/2.5/weather?q=";
 
-  var displayFar = true;
- $http.jsonp("http://ipinfo.io/json?callback=JSON_CALLBACK")
+  var displayFahrenheit = true;
+  $http.jsonp("http://ipinfo.io/json?callback=JSON_CALLBACK")
         .success(function(data){
           vm.location = data.city;
           vm.country = data.country;
           $http.jsonp(api + vm.location + "," + vm.country + "&units=metric" + "&APPID=061f24cf3cde2f60644a8240302983f2" + "&callback=JSON_CALLBACK")
                 .success(function(data){
                   vm.weather = data.weather[0].main;
-                  console.log(vm.weather);
                   vm.temperature = Math.round(data.main.temp);
                   vm.temperatureFar = Math.round(vm.temperature * 1.8 + 32);
-
-                  if (vm.weather === "Rain"){
-                    $('body').attr('id','rain');
-                  }
-                  else if(vm.weather === "Clouds"){
-                    $('body').attr('id','cloud');
-                    $('body').css('color','white');
-                  }
-                  else if(vm.weather === "Snow"){
-                    $('body').attr('id','snow');
-                    $('body').css('color','black');
-                  }
-                  else if(vm.weather === "Sunny"){
-                    $('body').attr('id','sun');
-                  }
+                  changeBackgroundImage(vm.weather);
                 });
         });
 
-  vm.changeTemp = function(){
-    if (displayFar){
+  function changeBackgroundImage(apiWeather){
+    var weather = apiWeather.toLowerCase();
+    switch(weather){
+      case 'rain' || 'drizzle' || 'thunderstom':
+        addWeatherElements("rain","white");
+        break;
+      case 'clouds':
+        addWeatherElements("cloud","white");
+        break;
+      case 'snow':
+        addWeatherElements("snow","black");
+        break;
+      case 'clear':
+        addWeatherElements("sun","black");
+        break;
+    }
+  }
+
+  function addWeatherElements(weather, color){
+    $('body').attr('id',weather);
+    $('body').css('color',color);
+  }
+
+  vm.updateTemperatureView = function(){
+    if (displayFahrenheit){
       $('#temp').html(vm.temperatureFar +"&#8457;");
-      displayFar = false;
+      displayFahrenheit = false;
     }
     else{
       $('#temp').html(vm.temperature +"&#8451;");
-      displayFar = true;
+      displayFahrenheit = true;
     }
-  }
-  vm.fetchRandomQuote = function(){
-    $http.jsonp(url)
-        .success(function(data){
-          vm.randomQuote = data[0].content.replace('<p>','').replace('</p>','');
-          vm.author = data[0].title;
-          $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='
-          + encodeURIComponent('"' + vm.randomQuote + '" ' + vm.author));
-    });
-  }
+  };
 });
